7551e428a306e51717039a769edc18cc779824f3
jhrv <johnny.horvi@gmail.com>
2013-02-23 15:47:32 +0100

    removed example and configured a simple get method for nodes (super simple!)

--- code/app/controllers/Application.scala
+++ code/app/controllers/Application.scala
@@ -2,11 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import model.Node
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+  def nodes = Action {
+    val myNode = new Node
+    myNode.name = "server1"
+
+    Ok(myNode.name)
   }
   
 }
